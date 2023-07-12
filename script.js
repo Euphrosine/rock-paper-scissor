255a3f1aaa9ef4bcc3cb6fd5ff2166e14f6391a2
Euphrosine <euphrosine46@gmail.com>
2023-07-12 01:59:42 -0400

    function that randomly displays computer's choice created

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+getComputerChoice();
+
+
